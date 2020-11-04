41ed32f2d6f37013db219f2c0e76be0a1119283c
sriutaminingsih <49768122+tamiutami@users.noreply.github.com>
2020-11-05 03:23:39 +0700

    Add files via upload

---
+++ PBOIF2_10116378_LATIHAN18_Kelipatan/src/pboif2_10116378_latihan18_kelipatan/PBOIF2_10116378_LATIHAN18_Kelipatan.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2_10116378_latihan18_kelipatan;
+
+/**
+ *
+ * @author Sri Utami Ningsih
+ * Nama  : Sri Utami Ningsih
+ * Kelas : PBO-IF2
+ * Nim   : 10116378
+ */
+public class PBOIF2_10116378_LATIHAN18_Kelipatan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        for (double i = 3.5; i <= 35; i += 3.5){
+         System.out.println(i);
+     }
+    }
+    
+}
